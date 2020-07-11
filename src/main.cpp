8beefe6519305cf1e8dcd91bb924719c68e64bc2
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-11 22:09:34 +0800

    update camera

--- src/main.cpp
+++ src/main.cpp
@@ -162,8 +162,19 @@ int main() {
 
 //        glm::mat4 model = glm::mat4(1.0f);
 //        model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-        glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+//        glm::mat4 view = glm::mat4(1.0f);
+//        view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f),
+//                           glm::vec3(0.0f, 0.0f, 0.0f),
+//                           glm::vec3(0.0f, 1.0f, 0.0f));
+
+
+        float radius = 10.0f;
+        float camX = sin(glfwGetTime()) * radius;
+        float camZ = cos(glfwGetTime()) * radius;
+        glm::mat4 view;
+        view = glm::lookAt(glm::vec3(camX, 0.0, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 1.0, 0.0));
+
+
         glm::mat4 projection = glm::mat4(1.0f);
         projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / WINDOW_HEIGHT, 0.1f, 100.0f);
 
