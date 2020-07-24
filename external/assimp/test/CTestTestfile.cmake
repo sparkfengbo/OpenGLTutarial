997ded2b12ba03fb5dc0c09d6ec1e96d61cffbb9
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-24 09:11:39 +0800

    update assimp

---
+++ external/assimp/test/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /Users/fengbo/CLionProjects/OpenGLTutarial/external/assimp/test
+# Build directory: /Users/fengbo/CLionProjects/OpenGLTutarial/external/assimp/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unittests "unit")
+set_tests_properties(unittests PROPERTIES  _BACKTRACE_TRIPLES "/Users/fengbo/CLionProjects/OpenGLTutarial/external/assimp/test/CMakeLists.txt;164;add_test;/Users/fengbo/CLionProjects/OpenGLTutarial/external/assimp/test/CMakeLists.txt;0;")
+subdirs("headercheck")
---
+++ external/assimp/test/headercheck/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/fengbo/CLionProjects/OpenGLTutarial/external/assimp/test/headercheck
+# Build directory: /Users/fengbo/CLionProjects/OpenGLTutarial/external/assimp/test/headercheck
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
