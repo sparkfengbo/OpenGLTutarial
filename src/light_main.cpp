7a212c4d64f0a9a9f082e489e33cddfd828497f8
fengbo.hit <fengbo.hit@bytedance.com>
2020-07-24 17:32:44 +0800

    update light position

--- src/light_main.cpp
+++ src/light_main.cpp
@@ -39,7 +39,7 @@ float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
 // lighting
-glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+glm::vec3 lightPos(0.0f, 0.0f, 6.0f);
 
 const float vertices[] = {
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
